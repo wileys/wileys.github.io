702ff315f432d0afbe475cef8dc3ab50e6009102
wileys <waileasiler@yahoo.com>
2016-07-21 10:38:27 -0700

    Added to Jquery Effects

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,35 @@
 
 $(document).ready(function(){
-	var off = false;
-	var clicked = false;
 	$(".about").hide();
+
+
+	$("#clicky").click(function(){
+		var $toggle = $(this);
+		var $H2 = $("h2");
+		$(".about").toggle(function () {
+			$H2.text(($(".about").is(':visible') ? 'CLOSE' : 'OPEN') + ' ABOUT ME');
+			});
+		$(".portrait").css({
+				"width" : "100px",
+				"height" : "100px"
+			});
+
+			
+
+	$("#clickhere").click(function(){
+		var $portfolyo = "#portfolyo";
+		$($portfolyo).toggle();
+		
+
+
+	});
 	
-	while (off === false) {
+	});
+	/*$('#clicky').click(function(){
+				$(".about").hide();
+				alert("yo");
+	});
+	/*while (off === false) {
 		if (clicked === false) {
 			$("#clicky").click(function(){
 				$(".about").show();
@@ -23,7 +48,5 @@ $(document).ready(function(){
 				$(".about").hide();
 				clicked = false;
 				return clicked;
-			});
-		};
-	};
+			});*/
 });
